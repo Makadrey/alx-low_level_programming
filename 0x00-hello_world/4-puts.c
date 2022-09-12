bb5ddd6b606473f4418b8c4b8d99e67a79c1be3b
Makadrey <oluwadamilareadubi@gmail.com>
2022-09-12 16:25:13 -0700

    tasl

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
@@ -9,8 +10,7 @@
  */
 
 int main(void)
-
-{	
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
